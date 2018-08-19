54507aa13728dfc7683ba1cda262fa68664ffdfd
Adam Mahood <mahood.adam@gmail.com>
2018-08-18 20:24:19 -0400

    lint: styling changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    std::cout << "Hello " << (argc == 1 ? "World" : argv[1]) << "!";
+  std::cout << "Hello " << (argc == 1 ? "World" : argv[1]) << "!";
 
-    return 0;
+  return 0;
 }
